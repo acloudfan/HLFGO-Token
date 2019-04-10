b8ee05d7fb452e594b55179d7da0b0a0c8de3e0d
vagrant <vagrant@vagrant.vm>
2019-04-10 15:08:21 +0000

    clean

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Used for cleaning the project
+rm -rf ./cid/vendor
+rm -rf ./history/vendor
+rm -rf ./qry/v1/vendor
+rm -rf ./qry/v2/vendor
+rm -rf ./qry/v3/vendor
+rm -rf ./qry/v4/test/vendor
